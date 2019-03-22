206f9028e5d8e12d52f526651eae8f3f47bb2eb5
Ilya <ilya.shelepov2000@gmail.com>
2019-03-22 21:04:38 +0300

    markups (keyboard fix)

--- markups.py
+++ markups.py
@@ -6,7 +6,8 @@ Created on Fri Mar 22 20:16:21 2019
 """
 from telebot import types
 
-start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True,
+                                         one_time_keyboard=True)
 start_markup_btn = types.KeyboardButton('/start')
 find_markup_btn = types.KeyboardButton('/find')
 start_markup.add(start_markup_btn,find_markup_btn)
