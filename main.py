c9e8405d8bd41623c4a8a2f7b83fa2acbde9b982
Ilya <ilya.shelepov2000@gmail.com>
2019-03-22 20:58:07 +0300

    fix

--- main.py
+++ main.py
@@ -1,93 +1,98 @@
-import requests
-from pprint import pprint
-import os
-import apteka103by as apteka
 
-
-class SimpleBot:
-    def __init__(self, token):
-        self.__token = token
-        self.__api_url = "https://api.telegram.org/bot{}/".format(token)
-        self.__offset = None
-        
-    def get_updates(self, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': self.__offset}
-        resp = requests.get(self.__api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-    
-    def get_last_update(self):
-        updates = self.get_updates()
-
-        while updates == []:
-            updates = self.get_updates()
-
-        last_update = updates[-1]
-        self.__offset = last_update['update_id'] + 1
-        return updates[-1]
-    
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.__api_url + method, params)
-        return resp
-
-def main():
-    TOKEN = os.environ.get('TOKEN',None)
-    if TOKEN == None:
-        file = open('./config.txt')
-        TOKEN = file.read()
-        file.close()
-    my_bot = SimpleBot(TOKEN)
-#    second_upd = None
-    while True:
-#        if second_update == None:
-#            first_upd = my_bot.get_last_update()
-#        else:
-#            first_upd = second_upd
-        first_upd = my_bot.get_last_update()
-        pprint(first_upd)
-        first_upd_text = first_upd['message']['text']
-        first_upd_id = first_upd['message']['chat']['id']
-        try:
-            drugs = apteka.find_drugs(first_upd_text)
-        except Exception as e:
-            my_bot.send_message(first_upd_id,e)
-            continue
-        drug = drugs[0]
-        drug_id = apteka.get_drug_id(drug['url'])
-#        message = apteka.get_result(drug_id)
-#        my_bot.send_message(first_upd_id,message)
-        result_array = apteka.get_result(drug_id)
-        
-        for item in result_array:    
-            my_bot.send_message(first_upd_id,item)
-        
-        if result_array[-1] == None:
-            my_bot.send_message(first_upd_id,'Нет дополнительной информации \
-                                по препарату. \
-                                Возможно, препарат отсутствует в продаже \n \
-                                или отсутствует описание в принципе \n \
-                                Попробуйте один из вариантов:')
-            message = ''
-            for i in range(len(drugs)):
-                message +=( str(i) + '.' + drugs[i]['title']+'\n')
-            my_bot.send_message(first_upd_id,message)
-                                        
-#        second_upd = my_bot.get_last_update()
-#        second_upd_id = second_upd['message']['chat']['id']
-#        if second_upd_id != first_upd_id:
-#            continue            
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
-        
-        
 #%%
+
+import os
+import telebot
+import apteka103by as parser
+from Task import Task
+import markups as m
+
+TOKEN = os.environ.get('TOKEN',None)
+if TOKEN == None:
+    file = open('./config.txt',mode='r')
+    TOKEN = file.read()
+    file.close()
+
+bot = telebot.TeleBot(TOKEN)
+task = Task()
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id , 'Привет, когда я вырасту, я буду \
+                     парсить сайт 103.by')
+    if not task.isRunning:
+        chat_id = message.chat.id
+        msg = bot.send_message(chat_id, 'Сколько тебе лет?')
+        bot.register_next_step_handler(msg, askAge) #askSource
+        task.isRunning = True
+
+def askAge(message):
+    chat_id = message.chat.id
+    text = message.text
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Возраст должен быть числом, введи ещё раз.')
+        bot.register_next_step_handler(msg, askAge) #askSource
+        return
+    bot.send_message(chat_id, 'Спасибо, я запомнил что тебе ' +
+                     text + ' лет.', reply_markup=m.start_markup)
+    task.isRunning = False
+
+@bot.message_handler(commands=['find'])
+def find_handler(message):
+    msg = bot.send_message(message.chat.id,'Какое лекарство будем искать?')
+    bot.register_next_step_handler(msg,find_drugs)
+    task.isRunning = True
+
+def find_drugs(message):
+    chat_id = message.chat.id
+    try:
+        task.drugs = parser.find_drugs(message.text)
+    except Exception as e:
+        bot.send_message(chat_id, e)
+        task.isRunning = False
+    else:
+        mess = ''
+        for i in range(len(task.drugs)):
+            mess += str(i)+'. ' + task.drugs[i]['title'] + '\n'
+        msg = bot.send_message(message.chat.id, mess)
+        bot.register_next_step_handler(msg,make_choice_and_find_drug)
+        task.isRunning = True
+
+def make_choice_and_find_drug(message):
+    chat_id = message.chat.id
+    text = message.text
+    if not text.isdigit() or not (int(text)>=0 and int(text)<len(task.drugs) ) :
+        msg = bot.send_message(chat_id,'Введи число от 0'+ ' до '+
+                               str(len(task.drugs)-1))
+        bot.register_next_step_handler(msg, make_choice_and_find_drug)
+        return
+    choice = int(text)
+    drug_url_piece = task.drugs[choice]['url']
+    drug_id = parser.get_drug_id(drug_url_piece)
+    result_array = parser.get_result(drug_id)
+    mess = ''
+    for item in result_array:    
+        mess += (item + '\n'+'\n')#bot.send_message(chat_id,item)
+    bot.send_message(chat_id, mess,reply_markup=m.start_markup)
+    task.isRunning = False    
     
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - парсер 103.by .',reply_markup=m.start_markup)
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?',reply_markup=m.start_markup)
+    else:
+        bot.send_message(chat_id, 'Прости, я тебя не понял :(',reply_markup=m.start_markup)
+
+@bot.message_handler(content_types=['photo'])
+def text_handler(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Красиво.',reply_markup=m.start_markup)
     
+bot.polling(none_stop=True)
     
+#%%
